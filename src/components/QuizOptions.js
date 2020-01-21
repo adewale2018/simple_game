dafd79003a55e564a63a4476e0899eeaa677ee61
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-01-21 09:13:17 +0100

    Add option component

---
+++ src/components/QuizOptions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QuizOptions = () => {
+  return (
+    <div className='fields'>
+      <div className='field-block'>20</div>
+    </div>
+  );
+};
+
+export default QuizOptions;
