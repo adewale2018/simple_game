3258ea80e6a5094c956ccf6d2fcee1bec4bafbd6
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-01-22 19:54:34 +0100

    Add the main logic to the game

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -9,14 +9,45 @@ class Quiz extends Component {
     this.state = { riddle };
   }
   randomNumber(min, max) {
-    return Math.floor(Math.random() * (max-min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  generateRandomOptions(sum) {
+    // let result = sum;
+    let resultsArray = [];
+    let randomNumberArray = [];
+
+    while (randomNumberArray.length <= 3) {
+      let randomNumber = this.randomNumber(1, 19);
+      if (randomNumberArray.indexOf(randomNumber) > -1) continue;
+      randomNumberArray.push(randomNumber);
+    }
+    console.log(randomNumberArray);
+    for (let i = 0; i < 3; i++) {
+      let addSubtract = this.randomNumber(0, 1);
+      let result = sum;
+      if (addSubtract === 1) {
+        //add the number to the result
+        result += randomNumberArray[i];
+        resultsArray.push(result);
+      } else {
+        //subtract the number from the result
+        result -= randomNumberArray[i];
+        resultsArray.push(result);
+      }
+    }
+
+    return resultsArray;
   }
   playGame() {
     let field1 = this.randomNumber(20, 40);
-    let field2 = this.randomNumber(12, 33);
-    let result =  field1 + field2;
+    let field2 = this.randomNumber(20, 50);
+    let result = field1 + field2;
+    let resultsArray = this.generateRandomOptions(result);
+    resultsArray.push(result);
+    resultsArray.sort(function(a, b)  { return 0.5 - Math.random()});
+    console.log(resultsArray);
     let riddle = {
-      resultsArray: [6, 7, 8, 9],
+      resultsArray: resultsArray,
       field1: field1,
       field2: field2,
       answer: result
@@ -37,7 +68,8 @@ class Quiz extends Component {
       <div className='quiz'>
         <div className='quiz-content'>
           <p className='question'>
-            What is the sum of <span className='text-info'>{this.state.riddle.field1}</span> and{" "}
+            What is the sum of{" "}
+            <span className='text-info'>{this.state.riddle.field1}</span> and{" "}
             <span className='text-info'>{this.state.riddle.field2}</span> ?{" "}
           </p>
           {this.renderOptions()}
